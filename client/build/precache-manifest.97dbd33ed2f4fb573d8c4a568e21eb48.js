fa986f299bca17989b193bfb4f07d54ef803cda0
Walihossain <w.hossain@queensu.ca>
2019-11-02 23:19:23 -0400

    second try to deploy

---
+++ client/build/precache-manifest.97dbd33ed2f4fb573d8c4a568e21eb48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47414c1c73fac657d1b4bca4d699208f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b97ed6294d410929e8",
+    "url": "/static/css/main.baf58c04.chunk.css"
+  },
+  {
+    "revision": "5e7d92209895b94428d0",
+    "url": "/static/js/2.f940ff93.chunk.js"
+  },
+  {
+    "revision": "92b97ed6294d410929e8",
+    "url": "/static/js/main.5416d762.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1f98bed7529374ea8f71ea0b30397f2a",
+    "url": "/static/media/landingBackground1.1f98bed7.jpg"
+  },
+  {
+    "revision": "cf3d08da5dc3f7952647aee426964885",
+    "url": "/static/media/landingBackground2.cf3d08da.jpg"
+  },
+  {
+    "revision": "af811b94f7ed3f8c0556f9826dada1a2",
+    "url": "/static/media/profiledemopic.af811b94.png"
+  }
+]);
